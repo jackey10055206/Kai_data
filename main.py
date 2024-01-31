eab04b8aa0141c534b7e4ae6f8f3cd5b89fc0980
jackey10055206 <jackey10055206@gmail.com>
2024-01-31 19:35:29 +0800

    Update error detection

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from openpyxl import Workbook
 
-from process_data import machine_num, machine_series, file_dir, usecols, gas_column, Real_time, Machine_time, Rtime
+from process_data import machine_num, machine_series, file_dir, usecols, gas_column, gas_type, Real_time, Machine_time, Rtime
 
 start_col = 1
 
@@ -29,7 +29,6 @@ def process_real_time():
 
     df_new['TIME'] = pd.to_datetime(df_new['TIME'], format='%H:%M:%S', errors='coerce').dt.time
     df_new['DATE'] = pd.to_datetime(df_new['DATE'], format='%Y-%m-%d', errors='coerce')
-
     df_new['DATETIME'] = pd.to_datetime(df_new['DATE'].astype(str) + ' ' + df_new['TIME'].astype(str),
                                          errors='coerce')
 
@@ -37,9 +36,9 @@ def process_real_time():
         start_time = pd.to_datetime(Mtime[idx])
         start_time = start_time + timedelta(seconds=59)
         end_time = start_time + timedelta(minutes=3, seconds=1)
-
+       
         selected_data = df_new[(df_new["DATETIME"] > start_time) & (df_new["DATETIME"] < end_time)]
-
+    
         regression_results = []
         max_r2 = float('-inf')
         max_r2_record = []
@@ -55,40 +54,44 @@ def process_real_time():
 
             predicted_values = model.predict(window_time)
             r2 = r2_score(window_data, predicted_values)
-
+            
             if r2 > max_r2:
                 max_r2 = r2
                 max_r2_record = [{'start_time': selected_data['TIME'].iloc[i], 'a': a, 'r2': r2, 'window_data': window_data}]
-
+                
             regression_results.append({'start_time': selected_data['TIME'].iloc[i], 'a': a, 'b': b, 'r2': r2})
+        
+        
+        if not max_r2_record:
+            print(f"No record for time {current_time}")
+        else:           
+            max_r2_record = max_r2_record[0]  # 取出 dict 資訊
+            max_r2_start_time = max_r2_record['start_time']
+            max_r2_a = max_r2_record['a']
+            max_r2_r2 = max_r2_record['r2']
+            # 將 max_r2_start_time 轉換為時間
+            max_r2_start_time = datetime.combine(date.today(), max_r2_start_time)  # 假設日期是當天的日期
+            # 創建包含 max_r2_start_time 以及之後 119 秒的時間的列表
+            time_series = [max_r2_start_time + timedelta(seconds=i) for i in range(120)]
 
-        max_r2_record = max_r2_record[0]  # 取出 dict 資訊
-        max_r2_start_time = max_r2_record['start_time']
-        max_r2_a = max_r2_record['a']
-        max_r2_r2 = max_r2_record['r2']
-        # 將 max_r2_start_time 轉換為時間
-        max_r2_start_time = datetime.combine(date.today(), max_r2_start_time)  # 假設日期是當天的日期
-        # 創建包含 max_r2_start_time 以及之後 119 秒的時間的列表
-        time_series = [max_r2_start_time + timedelta(seconds=i) for i in range(120)]
+            # 寫入 Start Time
+            sheet.cell(row=1, column=start_col + idx * 2, value=f"Start Time {idx + 1}")
 
-        # 寫入 Start Time
-        sheet.cell(row=1, column=start_col + idx * 2, value=f"Start Time {idx + 1}")
+            # 寫入時間序列和濃度值
+            for i, (time, concentration) in enumerate(zip(time_series, max_r2_record['window_data'])):
+                sheet.cell(row=i + 2, column=start_col + idx * 2, value=time.time())
+                sheet.cell(row=i + 2, column=start_col + idx * 2 + 1, value=float(concentration[0]))
 
-        # 寫入時間序列和濃度值
-        for i, (time, concentration) in enumerate(zip(time_series, max_r2_record['window_data'])):
-            sheet.cell(row=i + 2, column=start_col + idx * 2, value=time.time())
-            sheet.cell(row=i + 2, column=start_col + idx * 2 + 1, value=float(concentration[0]))
+            # 寫入 a 和 R^2
+            sheet.cell(row=122, column=start_col + idx * 2, value=f"a {idx + 1}")
+            sheet.cell(row=123, column=start_col + idx * 2, value=max_r2_a)
+            sheet.cell(row=124, column=start_col + idx * 2, value=f"R^2 {idx + 1}")
+            sheet.cell(row=125, column=start_col + idx * 2, value=max_r2_r2)
 
-        # 寫入 a 和 R^2
-        sheet.cell(row=122, column=start_col + idx * 2, value=f"a {idx + 1}")
-        sheet.cell(row=123, column=start_col + idx * 2, value=max_r2_a)
-        sheet.cell(row=124, column=start_col + idx * 2, value=f"R^2 {idx + 1}")
-        sheet.cell(row=125, column=start_col + idx * 2, value=max_r2_r2)
-
-    # 設定儲存格格式
-    for row in sheet.iter_rows(min_row=1, max_row=125, min_col=start_col, max_col=start_col + len(Rtime) * 2):
-        for cell in row:
-            cell.alignment = Alignment(wrap_text=True, vertical='center', horizontal='center')
+            # 設定儲存格格式
+            for row in sheet.iter_rows(min_row=1, max_row=125, min_col=start_col, max_col=start_col + len(Rtime) * 2):
+                for cell in row:
+                    cell.alignment = Alignment(wrap_text=True, vertical='center', horizontal='center')
 
 # 創建 Workbook
 wb = Workbook()
@@ -98,6 +101,6 @@ sheet = wb.active
 process_real_time()
 
 # 儲存 Excel
-output_excel = f"{machine_num}_{machine_series}_{Rtime[0][5:7]}_{Rtime[0][8:10]}_{gas_column[0]}.xlsx"
+output_excel = f"{machine_num}_{machine_series}_{Rtime[0][5:7]}_{Rtime[0][8:10]}_{gas_column[0]}_{gas_type}.xlsx"
 wb.save(output_excel)
 print("輸出成功")
